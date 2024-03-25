f80e4934535e8b9b6c7c36a6c0fa0d323589c494
Aakansha Thakur <98897384+AakanshaThakur@users.noreply.github.com>
2024-03-25 20:35:53 +0000

    datatype Summary

--- .devcontainer/basics/conversionOperations.js
+++ .devcontainer/basics/conversionOperations.js
@@ -23,5 +23,42 @@ let isLoggedIn = "hhh"
 let someNumber = 33
 
 let stringNmber = String(someNumber)
-console.log(stringNmber);
-console.log(typeof stringNmber)
+// console.log(stringNmber);
+// console.log(typeof stringNmber)
+
+// ****************************************Operations*****************************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " aakansha"
+
+let str = str1 + str2
+// console.log(str);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2+ 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
